cef569edc531ab5b04608bacf93a526dde89891c
yansongda <me@yansongda.cn>
2022-01-25 15:05:10 +0800

    update: 更新配置文件

--- config/pay.php
+++ config/pay.php
@@ -7,46 +7,59 @@ use Yansongda\Pay\Pay;
 return [
     'alipay' => [
         'default' => [
-            // 支付宝分配的 app_id
+            // 必填-支付宝分配的 app_id
             'app_id' => '',
-            // 应用私钥
+            // 必填-应用私钥 字符串或路径
             'app_secret_cert' => '',
-            // 应用公钥证书 路径
+            // 必填-应用公钥证书 路径
             'app_public_cert_path' => '',
-            // 支付宝公钥证书 路径
+            // 必填-支付宝公钥证书 路径
             'alipay_public_cert_path' => '',
-            // 支付宝根证书 路径
+            // 必填-支付宝根证书 路径
             'alipay_root_cert_path' => '',
             'return_url' => '',
             'notify_url' => '',
+            // 选填-服务商模式下的服务商 id，当 mode 为 Pay::MODE_SERVICE 时使用该参数
+            'service_provider_id' => '',
+            // 选填-默认为正常模式。可选为： MODE_NORMAL, MODE_SANDBOX, MODE_SERVICE
             'mode' => Pay::MODE_NORMAL,
         ],
     ],
     'wechat' => [
         'default' => [
-            // 公众号 的 app_id
-            'mp_app_id' => '',
-            // 小程序 的 app_id
-            'mini_app_id' => '',
-            // app 的 app_id
-            'app_id' => '',
-            // 商户号
+            // 必填-商户号，服务商模式下为服务商商户号
             'mch_id' => '',
-            // 合单 app_id
-            'combine_app_id' => '',
-            // 合单商户号
-            'combine_mch_id' => '',
-            // 商户秘钥
+            // 必填-商户秘钥
             'mch_secret_key' => '',
-            // 商户私钥
+            // 必填-商户私钥 字符串或路径
             'mch_secret_cert' => '',
-            // 商户公钥证书路径
+            // 必填-商户公钥证书路径
             'mch_public_cert_path' => '',
-            // 微信公钥证书路径
-            'wechat_public_cert_path' => [
-                '' => '',
-            ],
+            // 必填
             'notify_url' => '',
+            // 选填-公众号 的 app_id
+            'mp_app_id' => '',
+            // 选填-小程序 的 app_id
+            'mini_app_id' => '',
+            // 选填-app 的 app_id
+            'app_id' => '',
+            // 选填-合单 app_id
+            'combine_app_id' => '',
+            // 选填-合单商户号 
+            'combine_mch_id' => '',
+            // 选填-服务商模式下，子公众号 的 app_id
+            'sub_mp_app_id' => '',
+            // 选填-服务商模式下，子 app 的 app_id
+            'sub_app_id' => '',
+            // 选填-服务商模式下，子小程序 的 app_id
+            'sub_mini_app_id' => '',
+            // 选填-服务商模式下，子商户id
+            'sub_mch_id' => '',
+            // 选填-微信公钥证书路径, optional，强烈建议 php-fpm 模式下配置此参数
+            'wechat_public_cert_path' => [
+                '45F59D4DABF31918AFCEC556D5D2C6E376675D57' => __DIR__.'/Cert/wechatPublicKey.crt',
+            ],
+            // 选填-默认为正常模式。可选为： MODE_NORMAL, MODE_SERVICE
             'mode' => Pay::MODE_NORMAL,
         ],
     ],
