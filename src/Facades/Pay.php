bb8fe52c1a74c3f7fb9e34bc15244c187600ab9e
yansongda <me@yansongda.cn>
2025-02-09 18:45:58 +0800

    feat: 增加 unipay facade

--- src/Facades/Pay.php
+++ src/Facades/Pay.php
@@ -8,57 +8,38 @@ use Illuminate\Support\Facades\Facade;
 use Yansongda\Pay\Provider\Alipay;
 use Yansongda\Pay\Provider\Douyin;
 use Yansongda\Pay\Provider\Jsb;
+use Yansongda\Pay\Provider\Unipay;
 use Yansongda\Pay\Provider\Wechat;
 
 class Pay extends Facade
 {
-    /**
-     * Return the facade accessor.
-     *
-     * @return string
-     */
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return 'pay.alipay';
     }
 
-    /**
-     * Return the facade accessor.
-     *
-     * @return Alipay
-     */
     public static function alipay(): Alipay
     {
         return app('pay.alipay');
     }
 
-    /**
-     * Return the facade accessor.
-     *
-     * @return Wechat
-     */
     public static function wechat(): Wechat
     {
         return app('pay.wechat');
     }
 
-	/**
-	 * Return the facade accessor.
-	 *
-	 * @return Douyin
-	 */
 	public static function douyin(): Douyin
 	{
 		return app('pay.douyin');
 	}
 
-	/**
-	 * Return the facade accessor.
-	 *
-	 * @return Jsb
-	 */
 	public static function jsb(): Jsb
 	{
 		return app('pay.jsb');
+	}
+
+    public static function unipay(): Unipay
+	{
+		return app('pay.unipay');
 	}
 }
